4580294b12f198d2d1fd1097fd1c29d677a82d40
Victor Moura Dubeux <adv.victordubeux@gmail.com>
2023-03-30 14:22:36 -0300

    Commit 4
    Criei o primeiro script e o vinculei ao objeto "Car 1". Após isso, acessei o Visual Studio e codifiquei para imprimir no "console" uma frase.

---
+++ Udemy 1/Assets/PrintToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hey, mate! Are you well? I'm a good car, but my whells are bald...");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
